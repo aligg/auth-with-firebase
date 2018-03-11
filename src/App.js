804ef2997efa95aa4b7166e811c14a21a3fe8f85
Ali G <ali.glenesk@gmail.com>
2018-03-11 11:27:44 -0500

    added firebase and fixed lint errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-// import firebase from 'firebase'
+import firebase from 'firebase'
 import { Header } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
-  // componentWillMount() {
-  //   firebase.initializeApp({
-  //     apiKey: 'AIzaSyCTPU-41zRMzoC9aHc4tT-fjAoRWI1c1pQ',
-  //     authDomain: 'auth-198b9.firebaseapp.com',
-  //     databaseURL: 'https://auth-198b9.firebaseio.com',
-  //     projectId: 'auth-198b9',
-  //     storageBucket: '',
-  //     messagingSenderId: '945455102805'
-  //   })
-  // }
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCTPU-41zRMzoC9aHc4tT-fjAoRWI1c1pQ',
+      authDomain: 'auth-198b9.firebaseapp.com',
+      databaseURL: 'https://auth-198b9.firebaseio.com',
+      projectId: 'auth-198b9',
+      storageBucket: '',
+      messagingSenderId: '945455102805'
+    })
+  }
 
   render() {
     return (
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -44,7 +44,7 @@ class LoginForm extends Component {
     }
 
     return (
-      <Button onPress={this.onBottonPress.bind(this)}>
+      <Button onPress={this.onButtonPress.bind(this)}>
         Login
       </Button>
     )
